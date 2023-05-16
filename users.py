d3aacdb15c687a532b00ec7c85f2e5df9570ce3e
bavaaram <itsjustabavaar@gmail.com>
2023-05-16 17:55:41 +0330

    Add userame_check static method for checking repetitious usernames

--- users.py
+++ users.py
@@ -1,7 +1,7 @@
 #1 /usr/bin/python3
 
 class User:
-    all = []
+    all, all_usernames = [], []
 
     def __init__(self, username: str, password: str, phone_number: str=None):
         """
@@ -10,5 +10,12 @@ class User:
         self.username, self.__password = username, password
         self.phone_number = phone_number
         User.all.append(self)
-
-
+        
+    @staticmethod
+    def username_check(user_name: str) -> bool:
+        """
+        This static method actually for checking repetitious usernames
+        """
+        if user_name in User.all_usernames:
+            return False
+        return True
