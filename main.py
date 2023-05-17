c1054e300486702a836b2560a033ff1d1925645e
bavaaram <itsjustabavaar@gmail.com>
2023-05-18 00:16:59 +0330

    Add a state input bar for better navigation between pages and sections. add a section for exiting program and add a sign up form that using sign up method in User class for signing up.

--- main.py
+++ main.py
@@ -5,21 +5,22 @@ from users import User,UserError, PasswordError, TwoPasswordError, ShortPassword
 
 while 1:
     try:
-        stat = int(input())
+        stat = int(input("Stat (0(Exit) - 1(Sign Up) - 2(Sign In)):   "))
     except NameError:
         print("Invalid State! ")
         continue
 
-    if stat not in [0, 1, 2, 3, 4]:
-        print("Invalid Stat! ")
-        continue
-    if stat == 0:
+    if stat == "0":
+        print("Exiting the User Management Panel... ")
         break
-    if stat == 1:
-        username = input("Enter Username: ")
-        password = input("Enter Password: ")
-        phone_number = input("Enter Phone number(Optional): ")
-        User.signup(username, password, phone_number)
-    if stat == 2:
-        print("************** - Login form - **************")
-        username = input("Enter Username: ")
+
+    if stat == "1":
+        try:
+            username = input("Enter Username: ")
+            password = input("Enter Password: ")
+            phone_number = input("Enter Phone number(Optional): ")
+            User.signup(username, password, phone_number)
+        except UserError:
+            print("Username Already Taken! ")
+        except ShortPasswordError:
+            print("Too Short Password! ")
